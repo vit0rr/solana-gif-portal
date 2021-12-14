64ccb86f4950cafbf3fa938ec6579345a37d1cee
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-14 08:48:13 -0300

    feat: form gif

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
    * connected or not
    */
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('')
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -36,13 +37,28 @@ const App = () => {
 
   const connectWallet = async() => {
     const {solana} = window
-
+   
     if (solana){
       const response = await solana.connect()
       console.log('Connected with Public Key:', response.publicKey.toString())
       setWalletAddress(response.publicKey.toString())
+
+      
     }
   };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+  
+  const onInputChange = (event) => {
+    const { value } = event.target
+    setInputValue(value)
+  }
 
   const TEST_GIFS = [
     'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
@@ -62,6 +78,13 @@ const App = () => {
 
   const renderConnectedContainer = () => (
     <div className="connected-container">
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        sendGif()
+      }}>
+        <input type='text' placeholder='Enter gif link!' value={inputValue} onChange={onInputChange}></input>
+        <button type='submit' className="cta-button submit-gif-button">Submit</button>
+      </form>
       <div className="gif-grid">
         {TEST_GIFS.map(gif => (
           <div className="gif-item" key={gif}>
