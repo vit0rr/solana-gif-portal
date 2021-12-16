56f159b5a342677b9676c762366020e83e49a0df
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-16 08:49:44 -0300

    feat: render wallet button and acessing user account

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-/*
- * We are going to be using the useEffect hook!
-  Code based on buildspace
- */
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react'
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+
+// Constants
+const TWITTER_HANDLE = 'vit0rrk';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
 
 const App = () => {
-  /*
-   * This function holds the logic for deciding if a Phantom Wallet is
-   * connected or not
-   */
   const [walletAddress, setWalletAddress] = useState(null);
-  const [inputValue, setInputValue] = useState("");
-  const [gifList, setGifList] = useState([]);
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
 
       if (solana) {
         if (solana.isPhantom) {
-          console.log("Phantom wallet found!");
-          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log('Phantom wallet found!');
+          const response = await solana.connect({onlyIfTrusted: true})
           console.log(
-            "Connected with Public Key",
+            'Connected with Public Key:',
             response.publicKey.toString()
-          );
-          setWalletAddress(response.publicKey.toString());
+          )
+          setWalletAddress(response.publicKey.toString())
         }
       } else {
-        alert("Solana object not found! Get a Phantom Wallet 👻");
+        alert('Solana object not found! Get a Phantom Wallet 👻');
       }
     } catch (error) {
       console.error(error);
@@ -37,36 +33,13 @@ const App = () => {
 
   const connectWallet = async () => {
     const { solana } = window;
-
+  
     if (solana) {
       const response = await solana.connect();
-      console.log("Connected with Public Key:", response.publicKey.toString());
+      console.log('Connected with Public Key:', response.publicKey.toString());
       setWalletAddress(response.publicKey.toString());
     }
   };
-
-  const sendGif = async () => {
-    if (inputValue.length > 0) {
-      console.log('Gif link:', inputValue);
-      setGifList([...gifList, inputValue]);
-      setInputValue('');
-    } else {
-      console.log('Empty input. Try again.');
-    }
-  };
-
-  const onInputChange = (event) => {
-    const { value } = event.target;
-    setInputValue(value);
-  };
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const TEST_GIFS = [
-    "https://media.giphy.com/media/I6wUi5eTdUCWI/giphy.gif",
-    "https://media.giphy.com/media/OkhbcKxPDCKli/giphy.gif",
-    "https://media.giphy.com/media/XFnZJkgxvxvnG/giphy.gif",
-    "https://media.giphy.com/media/3fNmJ20ErpkjK/giphy.gif",
-  ];
 
   const renderNotConnectedContainer = () => (
     <button
@@ -75,73 +48,35 @@ const App = () => {
     >
       Connect to Wallet
     </button>
-  );
+  )
 
-  const renderConnectedContainer = () => (
-    <div className="connected-container">
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          sendGif();
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Enter gif link!"
-          value={inputValue}
-          onChange={onInputChange}
-        />
-        <button type="submit" className="cta-button submit-gif-button">
-          Submit
-        </button>
-      </form>
-      <div className="gif-grid">
-        {/* Map through gifList instead of TEST_GIFS */}
-        {gifList.map((gif) => (
-          <div className="gif-item" key={gif}>
-            <img src={gif} alt={gif} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-
-  /*
-   * When our component first mounts, let's check to see if we have a connected
-   * Phantom Wallet
-   */
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
     };
-    window.addEventListener("load", onLoad);
-    return () => window.removeEventListener("load", onLoad);
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
   }, []);
-
-  useEffect(() => {
-    if (walletAddress) {
-      console.log('Fetching GIF list...');
-      
-      // Call Solana program here.
-  
-      // Set state
-      setGifList(TEST_GIFS);
-    }
-  }, [TEST_GIFS, walletAddress]);
 
   return (
     <div className="App">
-      {/* This was solely added for some styling fanciness */}
-      <div className={walletAddress ? "authed-container" : "container"}>
-        <div className="container">
-          <div className="header-container">
-            <p className="header">🖼 GIF Portal</p>
-            <p className="sub-text">
-              View your GIF collection in the metaverse ✨
-            </p>
-            {!walletAddress && renderNotConnectedContainer()}
-            {walletAddress && renderConnectedContainer()}
-          </div>
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🖼 GIF Portal</p>
+          <p className="sub-text">
+            View your GIF collection in the metaverse ✨
+          </p>
+          {!walletAddress && renderNotConnectedContainer()}
+          <div className={walletAddress ? 'authed-container' : 'container'}></div>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
