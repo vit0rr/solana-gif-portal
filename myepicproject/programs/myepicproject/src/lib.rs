d732e2b8bf64a982dfcc15080f764f26d3f2ae27
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-17 13:18:11 -0300

    feat: Set up Vec

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -12,9 +12,17 @@ pub mod myepicproject { // pub mod é um modulo e nos permite definir varias fun
         Ok(())
     }
 
-    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult { // Funcao que recebe o contexto e produz o resultado
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult { // Funcao que recebe o contexto e produz o resultado
         // pega referencia da conta
         let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user
+
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key
+        }
+
+        base_account.gif_list.push(item); // Adiciona o gif na lista
         base_account.total_gifs += 1; // Adiciona um gif
         Ok(())
     }
@@ -34,10 +42,19 @@ pub struct StartStuffOff<'info> {
 pub struct AddGif<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
 }
 
 // Falar pra Solana o que nós queremos armazenar na conta
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
+    pub gif_list: Vec<ItemStruct>,
 }
