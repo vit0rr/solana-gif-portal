26136837b2fa45564f7a34e207588e1d4232f4ca
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-17 09:34:05 -0300

    feat: basic solana program

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -1,14 +1,15 @@
-use anchor_lang::prelude::*;
+use anchor_lang::prelude::*; // Import de algumas coisas
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"); // Declara um id
 
-#[program]
-pub mod myepicproject {
+#[program] // Define o programa - isso nos permite chamar no frontend dps
+pub mod myepicproject { // pub mod é um modulo e nos permite definir varias funcoes e variaveis
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult { 
+        // Funcao que recebe o contexto e produz o resultado 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize {} // aqui ele chama a funcao, que dentro dela, executa o Ok(())
