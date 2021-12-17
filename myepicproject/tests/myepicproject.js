526a1ee008b665d199db2f9899b4fb4f8fcc98d7
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-17 11:19:10 -0300

    feat: retrieve account data

--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -1,13 +1,30 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = require('@solana/web3.js');
 
 const main = async() => {
   console.log("🚀 Starting test...")
 
+  const provider = anchor.Provider.env()
   anchor.setProvider(anchor.Provider.env());
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.initialize(); // compila automaticamente nosso código lib.rs e implementa (essa é a funcao initialize la do lib.rs)
+
+  const baseAccount = anchor.web3.Keypair.generate()
+
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  })
 
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log('👀 GIF Count', account.totalGifs.toString())
+ /*  const tx = await program.rpc.initialize(); */ // compila automaticamente nosso código lib.rs e implementa (essa é a funcao initialize la do lib.rs)
+
 }
 
 const runMain = async () => {
